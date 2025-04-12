db297afd30f5cb93b3bdc25499e253dacf2fc69e
Argon_42 <79612731370@yandex.ru>
2025-04-12 18:49:54 +0300

    Add: Добавлен скролл на главном экране

---
+++ Assets/Scripts/Game/RotatingTextList.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace ZeroStats.Game
+{
+    public class RotatingTextList : MonoBehaviour
+    {
+        [SerializeField] private RectTransform container = default!;
+        [SerializeField] private TMP_Text textPrefab = default!;
+        [SerializeField] private float scrollSpeed = 50f;
+        [SerializeField] private TextProvider textProvider = default!;
+
+        private readonly List<TMP_Text> _items = new();
+        private float _elementHeight;
+        private int _count;
+        private Bounds _containerRect;
+
+        private void Start()
+        {
+            _elementHeight = textPrefab.GetComponent<RectTransform>().rect.height;
+            _count = Mathf.CeilToInt(container.rect.height / _elementHeight)+1;
+            _containerRect = RectTransformUtility.CalculateRelativeRectTransformBounds(container);
+
+            for (int i = 0; i < _count; i++)
+            {
+                var item = Instantiate(textPrefab, container);
+                item.text = textProvider.GetNextText();
+                item.rectTransform.localPosition = Vector2.up *
+                                                   (container.rect.height - i * _elementHeight -
+                                                    container.rect.height * 0.5f);
+                _items.Add(item);
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _items.Count; i++)
+            {
+                var item = _items[i];
+                item.rectTransform.localPosition += Vector3.up * (scrollSpeed * Time.deltaTime);
+                if (item.rectTransform.localPosition.y - _elementHeight > container.rect.height * 0.5f)
+                {
+                    item.text = textProvider.GetNextText();
+                    var last =_items.OrderBy(text => text.rectTransform.localPosition.y).First();
+                    item.rectTransform.localPosition = last.rectTransform.localPosition - Vector3.up * _elementHeight;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Game/TextProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroStats.Game
+{
+    public class TextProvider : MonoBehaviour
+    {
+        [SerializeField] private List<string> textValues = new();
+        private int _nextTextIndex = 0;
+
+        public string GetNextText()
+        {
+            if (textValues.Count == 0) return "Нет текста";
+            string next = textValues[_nextTextIndex];
+            _nextTextIndex = (_nextTextIndex + 1) % textValues.Count;
+            return next;
+        }
+    }
+}

6ccb0f9cbbaedb4365874768e35b48f031c016a5
Argon_42 <79612731370@yandex.ru>
2025-04-12 19:23:54 +0300

    Add: Добавлен список статов

--- Assets/Scripts/Game/TextProvider.cs
+++ Assets/Scripts/Game/TextProvider.cs
@@ -6,14 +6,11 @@ namespace ZeroStats.Game
     public class TextProvider : MonoBehaviour
     {
         [SerializeField] private List<string> textValues = new();
-        private int _nextTextIndex = 0;
 
         public string GetNextText()
         {
             if (textValues.Count == 0) return "Нет текста";
-            string next = textValues[_nextTextIndex];
-            _nextTextIndex = (_nextTextIndex + 1) % textValues.Count;
-            return next;
+            return $"{textValues[Random.Range(0, textValues.Count)]} - 0";
         }
     }
 }

045980cb77edd97e4604552d1f2fb065467a1475
Argon_42 <79612731370@yandex.ru>
2025-04-12 21:54:58 +0300

    Fix: Исправлен запуск проекта

--- Assets/Scripts/Boot/BootFlow.cs
+++ Assets/Scripts/Boot/BootFlow.cs
@@ -7,9 +7,11 @@ namespace ZeroStats.Boot
 {
     public class BootFlow : MonoBehaviour
     {
-        [SerializeField] private int delay = 1;
+        [SerializeField] private float delay = 0.3f;
 
-        private IEnumerable Start()
+        private void Start() => StartCoroutine(DelayedStart());
+
+        private IEnumerator DelayedStart()
         {
             yield return new WaitForSeconds(delay);
             SceneManager.LoadSceneAsync(Scenes.Game);

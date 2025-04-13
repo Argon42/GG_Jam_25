f4be03d164f5970c3c4efae02711de81a54346b9
Argon_42 <79612731370@yandex.ru>
2025-04-13 11:27:43 +0300

    Add: Добавлена анимация вращения карты при наведении мыши

--- Assets/Scripts/Game/CardView.cs
+++ Assets/Scripts/Game/CardView.cs
@@ -1,18 +1,26 @@
 ﻿using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using ZeroStats.Common;
 
 namespace ZeroStats.Game
 {
-    public class CardView : MonoBehaviour
+    public class CardView : MonoBehaviour, 
+        IPointerEnterHandler, IPointerExitHandler,
+        IPointerDownHandler, IPointerUpHandler
     {
         [SerializeField] private Image image = default!;
         [SerializeField] private TMP_Text text = default!;
         [SerializeField] private Button button = default!;
         [SerializeField] private DeltaEffectView deltaEffectViewPrefab = default!;
         [SerializeField] private RectTransform deltaEffectContainer = default!;
+        [SerializeField] private float angle = 10;
+        [SerializeField] private float speed = 5f;
+
+        private Coroutine? _enterCoroutine;
 
         public void Show(Card card, Func<Vector2> position, Action onSelect)
         {
@@ -26,6 +34,42 @@ namespace ZeroStats.Game
         {
             button.onClick.RemoveAllListeners();
             onEnd.Invoke();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _enterCoroutine = StartCoroutine(PointerEnter());
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            StopCoroutine(_enterCoroutine);
+            transform.rotation = Quaternion.identity;
+        }
+
+        private IEnumerator PointerEnter()
+        {
+            while (!destroyCancellationToken.IsCancellationRequested)
+            {
+                float time = 0f;
+                while (true)
+                {
+                    time += Time.deltaTime * speed;
+                    float z = Mathf.Sin(time) * angle;
+                    transform.rotation = Quaternion.Euler(0, 0, z);
+                    yield return null;
+                }
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
         }
     }
 }

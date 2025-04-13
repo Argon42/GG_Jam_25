bed2c8629e9656cbea67418e5e03c86676674d8c
Argon_42 <79612731370@yandex.ru>
2025-04-13 17:07:06 +0300

    Add: Добавлены звуки письма

---
+++ Assets/Scripts/Common/AnimationSoundEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZeroStats.Common
+{
+    public class AnimationSoundEventHandler : MonoBehaviour
+    {
+        [SerializeField] private AudioSource audioSource = default!;
+        [SerializeField] private List<AudioClip> clip = new();
+
+        public void PlaySound()
+        {
+            audioSource.clip = clip[Random.Range(0, clip.Count)];
+            audioSource.pitch = Random.Range(0.9f, 1.1f);
+            audioSource.Play();
+        }
+        
+    }
+}
